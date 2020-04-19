8d37ddb1c5ec7ad50ac084b18a1529beb1be2f6a
filip.cemb@gmail.com <filip.cemb@gmail.com>
2020-04-19 21:48:41 +0200

    init

---
+++ WSB_C_2020_Z1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSB_C_2020_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
