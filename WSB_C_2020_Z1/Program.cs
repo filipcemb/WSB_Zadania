eae1896029ee5957da5d23522c7d1a4e7078981a
filip.cemb@gmail.com <filip.cemb@gmail.com>
2020-04-28 19:01:31 +0200

    Zad: 1.2

--- WSB_C_2020_Z1/Program.cs
+++ WSB_C_2020_Z1/Program.cs
@@ -10,26 +10,26 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            long power = 1;
-            for (int i = 2; i <= 9; i++)
+            double power = 1;
+            for (byte i = 2; i <= 9; i++)
             {
                 power = power * i;
             }
             Console.WriteLine($"9! = {power}");
 
             power = 1;
-            for (int i = 2; i <= 20; i++)
+            for (byte i = 2; i <= 12; i++)
+            {
+                power = power * i;
+            }
+            Console.WriteLine($"12! = {power}");
+
+            power = 1;
+            for (byte i = 2; i <= 20; i++)   
             {
                 power = power * i;
             }
             Console.WriteLine($"20! = {power}");
-
-            power = 1;
-            for (int i = 2; i <= 243; i++)   // overflow :(
-            {
-                power = power * i;
-            }
-            Console.WriteLine($"243! = {power}");
             Console.ReadLine();
         }
     }
