9e8283a69d4dedc1f722d64d4b0658f4db16e906
filip.cemb@gmail.com <filip.cemb@gmail.com>
2020-04-28 20:03:27 +0200

    Zad: 1.3

--- WSB_C_2020_Z1/Program.cs
+++ WSB_C_2020_Z1/Program.cs
@@ -10,21 +10,16 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj liczbę: ");
-            int powerBase = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine($"{powerBase}! = {Power(powerBase)}");
-            Console.ReadLine();
-        }
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            int a = int.Parse(Console.ReadLine());
 
-        static double Power(int powerBase)
-        {
-            double power = 1;
-            for (int i = 2; i <= powerBase; i++)
-            {
-                power = power * i;
-            }
-            return power;
+            Console.WriteLine("Podaj drugą liczbę: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Twój wynik: ");
+            Console.WriteLine(a * b);
+
+            Console.ReadLine();
         }
     }
 }
