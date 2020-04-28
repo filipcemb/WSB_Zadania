4ff4aec7ebcf742f919bce9fa74d50cc5ea72da2
filip.cemb@gmail.com <filip.cemb@gmail.com>
2020-04-28 20:27:59 +0200

    Zad: 1.3.1

--- WSB_C_2020_Z1/Program.cs
+++ WSB_C_2020_Z1/Program.cs
@@ -16,9 +16,20 @@ namespace WSB_C_2020_Z1
             Console.WriteLine("Podaj drugą liczbę: ");
             int b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Twój wynik: ");
-            Console.WriteLine(a * b);
-
+            Console.WriteLine("Jaką operację chcesz wykonać (+/-): ");
+            string operation = Console.ReadLine();
+            if (operation == "+")
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(a - b);
+            }
+            else
+            {
+                Console.WriteLine("Nieznana operacja!");
+            }
             Console.ReadLine();
         }
     }
